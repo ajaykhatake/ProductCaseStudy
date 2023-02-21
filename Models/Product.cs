6bd064de17491e6adf4efccbf6e82a0097bb7020
ajayk <ajayk@DESKTOP-MCITRBU>
2023-02-21 13:09:03 +0530

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+
+namespace Mart.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductTitle { get; set; } = null;
+        public byte[] Image { get; set; }
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Price { get; set; }
+        public int Stocks { get; set; } 
+
+    }
+}
